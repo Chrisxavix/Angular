29d6be9b2ccd4ca245d61eae81894f31e898e475
Chris <chrischillogallid64@outlook.com>
2021-04-03 00:22:22 -0500

    feat: add ppes

---
+++ Pipes/README.md
@@ -0,0 +1,12 @@
+# Pipes
+Versión: 8.3.21.
+
+* Pipes como: 
+    * Currency.
+    * Decimal.
+    * Date.
+    * Porcentaje.
+    * Uppercase.
+    * Lowercase.
+    * Titlecase.
+    * Slice.
---
+++ Pipes/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { LOCALE_ID, NgModule } from '@angular/core';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import localeEs from '@angular/common/locales/es';
+import { registerLocaleData } from '@angular/common';
+
+registerLocaleData(localeEs)
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+  ],
+  providers: [
+    { provide: LOCALE_ID, useValue: 'es-Es'}
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
