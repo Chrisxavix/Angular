a6c8c8f97c3b91d20d47c8c2f8bafb465f64f469
Chris <chrischillogallid64@outlook.com>
2021-04-04 12:57:12 -0500

    feat: add material angular

--- Material/src/app/app.module.ts
+++ Material/src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule, MatInputModule } from '@angular/material';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,13 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    /* Modulos para usar material */
+    MatButtonModule,
+    MatInputModule,
+    /* Para usar Iconos */
+    MatIconModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
