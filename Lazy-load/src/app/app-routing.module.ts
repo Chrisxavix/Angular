eff5c6c7969f59e7e862ce304db1ffca6fc3bd6c
Chris <chrischillogallid64@outlook.com>
2021-04-04 01:42:45 -0500

    feat: add lazy-load

--- Lazy-load/src/app/app-routing.module.ts
+++ Lazy-load/src/app/app-routing.module.ts
@@ -1,11 +1,33 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
 
+const routes: Routes = [
+  {
+    path: 'customers',
+    loadChildren: () => import('./customers/customers.module').then(m => m.CustomersModule)
+  },
+  {
+    path: 'orders',
+    loadChildren: () => import('./orders/orders.module').then(m => m.OrdersModule)
+  },
+  {
+    path: '',
+    redirectTo: '',
+    pathMatch: 'full'
+  }
+];
 
-const routes: Routes = [];
-
+/* Usar un PreloadAllModules, para que los módulos vayan cargando en segund plano:
+{
+  enableTracing: true, // <-- debugging purposes only
+  preloadingStrategy: PreloadAllModules
+}  
+*/
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {
+    enableTracing: true, // <-- debugging purposes only
+    preloadingStrategy: PreloadAllModules
+  })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
