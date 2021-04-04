2f36689e0075898df275fbe005eacee8344c068b
Chris <chrischillogallid64@outlook.com>
2021-04-04 00:56:17 -0500

    test: add comments

--- Rutas/src/app/app-routing.module.ts
+++ Rutas/src/app/app-routing.module.ts
@@ -12,6 +12,9 @@ import { InicioComponent } from './componentes/inicio/inicio.component';
 import { NoPageComponent } from './componentes/no-page/no-page.component';
 
 const routes: Routes = [
+/* pathMatch: 'full' significa que toda la ruta URL debe coincidir y es consumida por el algoritmo de coincidencia de ruta.
+
+pathMatch: 'prefix' significa que se elige la primera ruta donde la ruta coincide con el inicio de la URL, pero luego el algoritmo de coincidencia de ruta continúa buscando rutas secundarias coincidentes donde coincide el resto de la URL. */
   { path: '', pathMatch: 'full', component: InicioComponent },
   { path: 'informacion', component: InfoComponent, canActivate: [AccionGuardGuard] },
   { path: 'acerca', component: AboutComponent },
