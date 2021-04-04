deeef50315a70f7eb7ac9b9b4b6417af11b580da
Chris <chrischillogallid64@outlook.com>
2021-04-04 03:07:43 -0500

    feat: add preload

--- Preload-module/src/app/app-routing.module.ts
+++ Preload-module/src/app/app-routing.module.ts
@@ -1,11 +1,30 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SelectivePreloadingStrategyService } from './selective-preloading-strategy.service';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'customers',
+    loadChildren: () => import('./customers/customers.module').then(m => m.CustomersModule),
+    /* Aquí le asigno que se cargará en preload con SelectivePreloadingStrategyService */
+    data: { preload: true}
+  },
+  {
+    path: 'orders',
+    loadChildren: () => import('./orders/orders.module').then(m => m.OrdersModule)
+  },
+  {
+    path: '',
+    redirectTo: '',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {
+    enableTracing: false, // <-- debugging purposes only Modulo para mapear las rutas o url
+    preloadingStrategy: SelectivePreloadingStrategyService
+  })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
