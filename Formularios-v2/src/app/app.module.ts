67864ec3db580e544bb58898fbc7a895b17d9bcb
Chris <chrischillogallid64@outlook.com>
2021-04-04 15:53:39 -0500

    feat: add form-group

--- Formularios-v2/src/app/app.module.ts
+++ Formularios-v2/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductFormComponent } from './components/product-form/product-form.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { FormGroupComponent } from './components/form-group/form-group.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductFormComponent,
+    FormGroupComponent,
   ],
   imports: [
     BrowserModule,
---
+++ Formularios-v2/src/app/components/form-group/form-group.component.ts
@@ -0,0 +1,54 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { debounceTime } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-form-group',
+  templateUrl: './form-group.component.html',
+  styleUrls: ['./form-group.component.scss']
+})
+export class FormGroupComponent implements OnInit {
+  form: FormGroup;
+  constructor(
+    private FormBuilder: FormBuilder,
+  ) {
+    /* Al usar con un this.FormBuilder me permite no estar creando con new:
+    name: new FormControl('', [Validators.required]), */
+
+    this.form = this.FormBuilder.group({
+      name: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(5)]],
+      date: ['', [Validators.required]],
+      email: ['', [Validators.email]],
+      text: ['', [Validators.maxLength(80)]],
+      category: ['', [Validators.required]],
+      gender: ['', [Validators.required]],
+    });
+
+    /* Visualizo cada cambio realizado en el formulario */
+    this.form.valueChanges.pipe(debounceTime(500))
+    .subscribe(value => {
+      console.log(value, 'value');
+    })
+  }
+
+  ngOnInit() {
+  }
+
+  save(event: Event) {
+    /* Evento por default de html es recargar la página, con el preventDefault
+    se asegura que no lo haga */
+    event.preventDefault();
+    if (this.form.valid) {
+      const value = this.form.value;
+      console.log(value, 'Excelente');
+    } else {
+      console.log('Error');      
+      this.form.markAllAsTouched();
+    }
+  }
+
+  doSomething() {
+    console.log('click');
+  }
+
+}
