0357c3ec1b0843b5ef96e7cd803c4f58d3c60716
chris <cxchillogalli@engicoders.com>
2021-04-20 20:24:49 -0500

    feat: add toast button

--- ToastNotification/src/app/app.module.ts
+++ ToastNotification/src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
+import { ToastTestComponent } from './toast-test/toast-test.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToastTestComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule, // required animations module
+    ToastrModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ ToastNotification/src/app/toast-test/toast-test.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+
+@Component({
+  selector: 'app-toast-test',
+  templateUrl: './toast-test.component.html',
+  styleUrls: ['./toast-test.component.scss']
+})
+export class ToastTestComponent implements OnInit {
+
+  constructor(
+    private toastr: ToastrService,
+  ) { }
+
+  ngOnInit() {
+  }
+
+  showSuccess() {
+    this.toastr.success('Hello world!', 'Toastr fun!');
+  }
+
+}
