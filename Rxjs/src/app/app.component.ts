24667c1179236ac92a0d78d95c6a19f988dde3b6
Chris <chrischillogallid64@outlook.com>
2021-06-26 13:56:24 -0500

    feat: add basic rxjs

--- Rxjs/src/app/app.component.ts
+++ Rxjs/src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { interval } from 'rxjs';
+import { mapTo } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'Rxjs';
+export class AppComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+    // this.intervalRxJs();        
+  }
+
+  /* Interval */
+  /* Actúa como un Settimeout */
+  intervalRxJs() {
+    const contador = interval(1000);
+    contador.subscribe(resp => {
+      console.log(resp, '????');
+      if (resp === 2) {
+        console.log('Valor de 2');
+      }
+    })
+  }
+
 }
